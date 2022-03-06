a91d0617828e30349a692acf27bade1e93764467
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-03-05 23:37:22 -0600

    Base functionality working, framework for stretch in place

--- scripts/client.js
+++ scripts/client.js
@@ -1,16 +1,22 @@
 $(readyNow);
 function readyNow() {
-    $(`#addEmployeeBtn`).on(`click`, addEmployee);
-    $(`tbody`).on(`click`, '#deleteBtn', deleteEmployee);
-    //$(`#deleteBtn`).on(`click`, deleteEmployee); wrong!
-
+    $(`#addEmployeeBtn`).on(`click`, addEmployee);//, storeData`
+    $(`tbody`).on(`click`, '#deleteBtn', deleteEmployee);//, removeSalary
+    // $(`#deleteBtn`).on(`click`, deleteEmployee); //wrong! The button doesn't exist yet and can't be targeted.
 }
 
-let staff = [];
-let totalRunningCost = 0;
+let staff = [];//seems helpful to create an array for later uses...
+
+const makeDollars = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+})
 
 
 function addEmployee() {
+
+
     let firstName = $(`#firstName`).val();
     let lastName = $(`#lastName`).val();
     let empNumber = $(`#empNumber`).val();
@@ -23,8 +29,9 @@ function addEmployee() {
         empNumber: empNumber,
         title: title,
         salary: salary
-    }
+    }//research: button ".data()" to help update the salary?
     staff.push(empObject);
+    currentSalary += salary;
 
     $(`#employeeTable`).append(`
             <tr>
@@ -32,35 +39,74 @@ function addEmployee() {
                 <td>${lastName}</td>
                 <td>${empNumber}</td>
                 <td>${title}</td>
-                <td>${salary}</td>
+                <td>${makeDollars.format(salary)}</td>
                 <td>
                 <button id="deleteBtn"> Remove Employee </button>
                 </td>
             </tr>
-            
             `)
+
     $(`input`).val('');
-    totalRunningCost = Number(totalRunningCost) + Number(salary);
-    $(`#totalCost`).empty();
-    $(`#totalCost`).append(totalRunningCost);
+    appendMonthlyCost();
+}
+
+function storeData() {
+    // $(document).ready(function(){
+    //     testObj = new Object();
+    //     testObj.greetingMorn = "Good Morning!";
+    //     testObj.greetingEve = "Good Evening!";
+    //     $("#btn1").click(function(){
+    //       $("div").data(testObj);
+    //     });
+    //     $("#btn2").click(function(){
+    //       alert($("div").data("greetingEve"));
+    //     });
+}
+
+let totalMonthlyCost = 0;
+let currentSalary = 0;//being populated from inside addEmployee
+
+
+function appendMonthlyCost() {
+    //console.log('in appendMonthlyCost');
+    totalMonthlyCost = (currentSalary / 12);
+    $(`#monthlyCostSpan`).empty();
+    // $(`#monthlyCostSpan`).removeClass('.noSalary');
+    // $(`#monthlyCostSpan`).addClass('.yesSalary');
+    $(`#monthlyCostSpan`).append(makeDollars.format(totalMonthlyCost));
+    currentSalary = 0;
     warningRed();
-
-    //research button ".data()"...to help update the salary?
-    // calc salary
-
 }
 
 function warningRed() {
-    if (Number(totalRunningCost > 20000)){
-$('#totalCost').toggleClass('warning');
+    //console.log('in warningRed');
+    if (totalMonthlyCost > 20000) {
+        $('#monthlyCostSpan').toggleClass('warning');
     }
 }
 
+function deleteEmployee() {
+    $(this).closest('tr').empty();
+}
 
-//***Note: potentially refactor addEmployee to call separate "appendTable"
+function removeSalary() {
+    // $(document).ready(function(){
+    //     testObj = new Object();
+    //     testObj.greetingMorn = "Good Morning!";
+    //     testObj.greetingEve = "Good Evening!";
+    //     $("#btn1").click(function(){
+    //       $("div").data(testObj);
+    //     });
+    //     $("#btn2").click(function(){
+    //       alert($("div").data("greetingEve"));
+    //     });
+}
+
+
+//***Note: potentially refactor addEmployee to call separate "appendTable"?
 // function appendTable() {
 //     //for (let person of staff) {
-//     $(`#employ`eeTable`).append(`
+//     $(`#employeeTable`).append(`
 //         <tr>
 //                 <td>${firstName}</td>
 //                 <td>${person.lastName}</td>
@@ -76,10 +122,25 @@ $('#totalCost').toggleClass('warning');
 //     $(`input`).val('');
 // }
 
+//the version below toggles between no formatting and "noSalary", 
+//but does not activate "yesSalary"
+
+// totalMonthlyCost = ((salary + totalMonthlyCost) / 12);
+//     $(`#monthlyCostSpan`).empty();
+//     $(`#monthlyCostSpan`).append(makeDollars.format(totalMonthlyCost));
+//     if ($('#monthlyCostSpan').val() === 0 || $('#monthlyCostSpan').val() === undefined) {
+//         $('#monthlyCostSpan').toggleClass('noSalary');
+//     }
+//     else {
+//         $('#monthlyCostSpan').toggleClass('yesSalary');
+//     }
+
+//this version toggles between "noSalary" and "yesSalary" (conditional didn't affect it)
+// if ($('#monthlyCostSpan').val() === 0 || $('#monthlyCostSpan').val() === undefined) {
+//     $('#monthlyCostSpan').toggleClass('noSalary');
+// }
+// else {
+//     $('#monthlyCostSpan').toggleClass('yesSalary');
+// }
 
 
-function deleteEmployee() {
-    $(this).closest('tr').empty();
-    console.log('in deleteBtn');
-
-}
